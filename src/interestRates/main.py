8f6388903c09cb835a7b213b45187c9d39cbd9f0
unknown <arlindojosboa@gmail.com>
2022-09-11 13:48:51 +0200

    IMPROVE: Remove test job

--- src/interestRates/main.py
+++ src/interestRates/main.py
@@ -7,20 +7,20 @@ def interestRatesUseCase():
     now = datetime.now()
     today = now.strftime('%Y-%m-%d')
 
-    last_date_in_DB_str = getLastUpdateDateDB()
-    last_date_in_DB = datetime.strptime(last_date_in_DB_str, '%Y-%m-%d')
+    last_date_in_DB = getLastUpdateDateDB()
+    date = datetime.strptime(last_date_in_DB, '%Y-%m-%d')
 
-    date = last_date_in_DB + timedelta(days=1)
     new_date = date.strftime('%Y-%m-%d')
 
     interestRates = []
 
     while today != new_date:
+        print(new_date)
+        date = date + timedelta(days=1)
+        new_date = date.strftime('%Y-%m-%d')
+
         interestRates = interestRatesService(new_date)
 
         saveInterestRatesDB(interestRates)
-
-        date = date + timedelta(days=1)
-        new_date = date.strftime('%Y-%m-%d')
 
     return interestRates
