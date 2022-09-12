081f0abf2b3b2a32575cea67ceb07a71c122f04c
unknown <arlindojosboa@gmail.com>
2022-09-12 17:10:40 +0200

    IMPROVE: Router

--- src/core/http/router/cpi.py
+++ src/core/http/router/cpi.py
@@ -1,5 +1,4 @@
 from fastapi import APIRouter
-from src.save import saveUseCase
 from src.cpi.main import cpiUseCase
 
 cpiRouter = APIRouter(
@@ -10,12 +9,6 @@ cpiRouter = APIRouter(
 @cpiRouter.get('')
 def controller():
   data = cpiUseCase()
-
-  return data
-
-@cpiRouter.get('/save')
-def controller():
-  data = saveUseCase()
 
   return data
   
