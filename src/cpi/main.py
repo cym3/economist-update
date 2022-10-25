ad98d7c14bea2bfa13a7b58fe3c50da08910eec5
Arlindo <arlindojosboa@gmail.com>
2022-10-26 00:56:03 +0200

    IMPROVE: CPI

--- src/cpi/main.py
+++ src/cpi/main.py
@@ -1,5 +1,3 @@
-from src.cpi.domain.entities.get_all import getAllDB
-from src.cpi.domain.entities.get_update import updateCpiDB
 from src.cpi.services.cpi import cpiService
 from src.cpi.domain.entities.save_cpi import saveCpiDB
 from src.cpi.domain.entities.get_last_update_date import getLastUpdateDateDB
@@ -7,26 +5,15 @@ from src.cpi.indicators import indicators
 
 def cpiUseCase():
     for indicator in indicators:
-        # last_update_date = getLastUpdateDateDB(indicator)
-        CPIs = getAllDB(indicator)
+        last_update_date = getLastUpdateDateDB(indicator)
 
-        newCPIs = []
-        for cpi in CPIs:
-            values: list = cpi['values']
+        CPI = cpiService(date=last_update_date, indicator=indicator)
 
-            cpi['values'] = values[:-1]
+        if CPI is not None:
+            saveCpiDB(CPIs=CPI, indicator=indicator)
+            print(CPI)
 
-            newCPIs.append(cpi)
-
-        updateCpiDB(newCPIs, indicator)
-
-        # CPI = cpiService(date=last_update_date, indicator=indicator)
-
-        # if CPI is not None:
-        #     saveCpiDB(CPIs=CPI, indicator=indicator)
-        #     print(CPI)
-
-        # else:
-        #     print('No new IPC to update')
+        else:
+            print('No new IPC to update')
 
     return 'Done'
